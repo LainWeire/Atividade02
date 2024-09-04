a3a32656e3643245bc7ac224a9853f06f1b65acf
LainWeire <lucasfrota3000@gmail.com>
2024-09-04 15:42:31 -0300

    Uff referencias

---
+++ atividade02.py
@@ -0,0 +1,4 @@
+#Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+Gc = float(input("Temperatura em Celsius: "))
+F = (Gc * (9 // 5) + 32)
+print (f"Graus Fahrenheint {F}")
